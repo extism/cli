27ebab95b4482d529bc65cf0792348a1ada46e8f
Steve Manuel <nilslice@gmail.com>
2022-09-03 16:37:28 -0600

    feat: echo install path with note about system path

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ dest=${1-$HOME/.local/bin}
 curl --fail https://raw.githubusercontent.com/extism/cli/main/extism_cli/__init__.py > /tmp/extism-cli
 mv /tmp/extism-cli "$dest"/extism
 chmod +x "$dest"/extism
+
+echo "\nInstalled `extism` to ${dest}. Please be sure that is in your system $PATH.\n"
