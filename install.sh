9d88ef48ee21e4283fba1b0cceb153c6172edb53
zach <zachshipko@gmail.com>
2022-12-05 19:33:18 -0800

    cleanup: remove pip install from install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ dest=${1-$HOME/.local/bin}
 curl --fail https://raw.githubusercontent.com/extism/cli/main/extism_cli/__init__.py > /tmp/extism-cli
 mv /tmp/extism-cli "$dest"/extism
 chmod +x "$dest"/extism
-pip3 install extism toml cffi --user
 
 echo "\nInstalled 'extism' to ${dest}. Please be sure that is in your system PATH.\n"
