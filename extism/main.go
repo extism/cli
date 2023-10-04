9b5afa1586ce15c40b3fd694955bd205e592cebe
zach <zach@dylibso.com>
2023-10-04 14:57:02 -0700

    chore: bump cli version

--- extism/main.go
+++ extism/main.go
@@ -19,7 +19,7 @@ var banner string = `
 func rootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "extism",
-		Version: "0.2.2",
+		Version: "0.3.0",
 		Long:    banner,
 		Short:   "A CLI for Extism, https://extism.org",
 	}
