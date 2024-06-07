d6dc05e578c97893ee29c0e7726ce7696bfb34be
Gavin Hayes <gavin@dylibso.com>
2024-06-07 14:33:53 -0400

    fix(install): create outprefix if it does not exist (#90)
    * fix(install): create outprefix if it does not exist
    
    * fix: check if directory exists first before mkdir
    
    * fix: write if statement correctly
    
    * ci: attempt to make lib install not flaky

--- scripts/get-extism.sh
+++ scripts/get-extism.sh
@@ -166,6 +166,10 @@ else
   reply=y
 fi
 if [ "$reply" = "y" ] || [ "$reply" = "Y" ] || [ "$reply" = "yes" ]; then
+  if [ ! -d "$out_prefix" ]; then
+    print "Creating directory $out_prefix"
+    $_sudo mkdir -p "$out_prefix"
+  fi
   if [ "$use_go" = "y" ]; then
     print "Installing using go install"
     GOOS=$os GOARCH=$arch go install "github.com/extism/cli/extism@$version" || err "Unable to install from source, make sure go is installed"
