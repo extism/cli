3e6a227e18538f66aeedee7f53f58cf18cbc9aa3
zach <zach@dylibso.com>
2024-06-11 12:49:40 -0700

    fix: add `-framework Security` to extism-static.pc on macOS (#94)
    * fix: add Security framework for macos
    
    * fix: use Libs instead of Libs.private

--- lib.go
+++ lib.go
@@ -260,6 +260,11 @@ func runLibInstall(cmd *cobra.Command, installArgs *libInstallArgs) error {
 							line = "prefix=" + installArgs.prefix + "\n"
 						}
 
+						// Inject `-framework Security` on macOS
+						if installArgs.os == "darwin" && strings.HasPrefix(line, "Libs:") {
+							line = strings.ReplaceAll(line, "Libs: ", "Libs: -framework Security ")
+						}
+
 						io.WriteString(out, line)
 					}
 					out.Close()
